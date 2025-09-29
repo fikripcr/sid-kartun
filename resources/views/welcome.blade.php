6a71993f2d4c4129608f3b84ce4a6acc78046942
fikripcr <fikri@pcr.ac.id>
2025-09-29 08:18:15 +0700

    ubah text klik disini pada welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,7 +54,7 @@
                     <h1 class="mb-1 font-medium">Let's get started</h1>
 
                     <!-- Contoh penggunaan route pada tag href -->
-                    <a href="{{ route('mahasiswa.show','HAHA') }}">Klik Disini</a>
+                    <a href="{{ route('mahasiswa.show','HAHA') }}">Silahkan Klik Disini</a>
 
                     <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">Laravel has an incredibly rich ecosystem. <br>We suggest starting with the following.</p>
                     <ul class="flex flex-col mb-4 lg:mb-6">
