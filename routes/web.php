6421b7b20a12124c782c75202cde946788d8c234
fikripcr <fikri@pcr.ac.id>
2025-09-29 10:11:17 +0700

    submit form pertanyaan

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\MahasiswaController;
 
 Route::get('/', function () {
@@ -24,3 +25,6 @@ Route::get('/nim/{param1?}', function ($param1 = 'Luffy') {
 });
 
 Route::get('/home',[HomeController::class,'index']);
+
+Route::post('question/store', [QuestionController::class, 'store'])
+		->name('question.store');
