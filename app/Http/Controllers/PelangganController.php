379f4ae3a4dfead4079a8106718d3daf5a8e9cb4
fikripcr <fikri@pcr.ac.id>
2025-11-10 10:44:59 +0700

    tes case sensitive

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -12,6 +12,7 @@ class PelangganController extends Controller
     public function index()
     {
         $data['dataPelanggan'] = Pelanggan::all();
+        
         return view('admin.pelanggan.index', $data);
     }
 
